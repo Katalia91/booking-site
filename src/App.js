26f077190308455f536336d439a4bd749d207d25
Natalia Kur <63752791+Katalia91@users.noreply.github.com>
2024-02-04 17:09:26 +0100

    Add basic config for eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>House Booking App</h1>
-      <p>coming soon</p>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>House Booking App</h1>
+				<p>coming soon</p>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
