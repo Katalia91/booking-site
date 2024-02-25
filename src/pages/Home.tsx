6a2fde13bdd9db0ff55cb4e3cd8a37db924ff5bc
Natalia Kur <63752791+Katalia91@users.noreply.github.com>
2024-02-25 11:48:23 +0100

    Add react-calendar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,31 @@
 import React from "react";
+import Calendar from "react-calendar";
 import Hero from "src/components/Hero";
+import { useState } from "react";
+import "react-calendar/dist/Calendar.css";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 function Home() {
+  const [value, onChange] = useState<Value>(new Date());
   return (
     <>
       <Hero
         title="Velkommen"
         subtitle="Her kan du reservere huset i San Pedro del Sol"
       />
-      <div className="calendar-wrapper">Calendar</div>
+      <div className="calendar-container">
+        <main className="calendar-content">
+          <Calendar
+            onChange={onChange}
+            locale="no"
+            showWeekNumbers
+            value={value}
+          />
+        </main>
+      </div>
       <div className="gallery">
         <figure>
           <img src="https://picsum.photos/id/17/680/380" alt="" />
